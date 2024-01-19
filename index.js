235d2858c38b6d6c9897d4cd702934cbd66d74bc
Mike <nguyen.mike1092@gmail.com>
2024-01-18 21:48:11 -0500

    log out responses

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// TODO: Include packages needed for this application
 const generateMarkdown = require(`./utils/generateMarkdown`);
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
 let init = async () => {
-	let inquirer = await require("inquirer");
-	inquirer
-		.prompt([
+	try {
+		const responses= await inquirer.prompt([
 			{
 				type: `input`,
 				message: `What is the title of your project?`,
@@ -15,68 +12,65 @@ let init = async () => {
 			},
 			{
 				type: `input`,
-				message: `Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide`,
+				message: `Provide a short description explaining the what, why, and how of your project.`,
 				name: `description`,
 			},
-			{
-                type: `input`,
-                message: `What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.`,
-                name: `install`
-            },
-            {
-                type:`input`,
-                message:`Provide instructions and examples for use.`,
-                name:`usage`,
-            },
-            {
-                type:`input`,
-                message:``,
-                name:`contributing`,
-            },
-            {
-                type:`input`,
-                message:``,
-                name:`test`,
-            },
-            {
-                type:`list`,
-                message:`Select a license`,
-                name:`license`,
-                choices:``
-            },
-            {
-                type:`input`,
-                message:`Please put your GitHub username.`,
-                name:`github`,
-            },
-            {
-                type:`input`,
-                message:`Please enter your email.`,
-                name:`email`,
-            },
-            {
-                type:``,
-                message:``,
-                name:`question`,
-            },
-		])
-		.then((answers) => {
-			writeToFile()
-		})
-		.catch((error) => {
-			if (error.isTtyError) {
-				console.log(`There was something wrong. IDK lol`);
-			} else {
-				console.log(`Success!`);
-			}
-		});
+			// {
+			// 	type: `input`,
+			// 	message: `What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.`,
+			// 	name: `install`,
+			// },
+			// {
+			// 	type: `input`,
+			// 	message: `Provide instructions and examples for use.`,
+			// 	name: `usage`,
+			// },
+			// {
+			// 	type: `input`,
+			// 	message: ``,
+			// 	name: `contributing`,
+			// },
+			// {
+			// 	type: `input`,
+			// 	message: ``,
+			// 	name: `test`,
+			// },
+			// {
+			// 	type: `list`,
+			// 	message: `Select a license`,
+			// 	name: `license`,
+			// 	choices: ``,
+			// },
+			// {
+			// 	type: `input`,
+			// 	message: `Please put your GitHub username.`,
+			// 	name: `github`,
+			// },
+			// {
+			// 	type: `input`,
+			// 	message: `Please enter your email.`,
+			// 	name: `email`,
+			// },
+			// {
+			// 	type: ``,
+			// 	message: ``,
+			// 	name: `question`,
+			// },
+		]);
+		await console.log(responses);
+        await writeToFile(responses)
+	} catch (error) {
+		console.log(`There was something wrong. IDK lol`);
+	}
 };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+function writeToFile(`README.md`, data) {
+    
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
 // Function call to initialize app
 init();
